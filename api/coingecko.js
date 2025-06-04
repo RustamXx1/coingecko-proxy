efbcfa07d24dc0f3e17315dc5dedaf34a7beac8f
RustamXx1 <164536554+RustamXx1@users.noreply.github.com>
2025-06-04 19:25:55 +0300

    Update coingecko.js

--- api/coingecko.js
+++ api/coingecko.js
@@ -1,10 +1,13 @@
 export default async function handler(req, res) {
   const { ids = "bitcoin", vs = "usd" } = req.query;
 
-  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=${vs}`;
-  const response = await fetch(url);
-  const data = await response.json();
-
-  res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
-  res.status(200).json(data);
+  try {
+    const url = `https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=${vs}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data from CoinGecko" });
+  }
 }
