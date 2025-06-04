7dd0af8809e9bb3e1aa5f1ba885d762610d7a207
RustamXx1 <164536554+RustamXx1@users.noreply.github.com>
2025-06-04 19:04:32 +0300

    Create coingecko.js

---
+++ api/coingecko.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  const { ids = "bitcoin", vs = "usd" } = req.query;
+
+  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=${vs}`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
+  res.status(200).json(data);
+}
